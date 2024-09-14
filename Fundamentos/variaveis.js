160295896e8587821d9c8e3fecf8100ade99d160
Alex Lima <alexsantoslima007@gmail.com>
2024-09-13 21:36:16 -0300

    status das variáveis

---
+++ Fundamentos/variaveis.js
@@ -0,0 +1,22 @@
+/**
+ * Estudo de constante e variáveis 
+ *  @author Alex Lima
+ */
+
+console.clear()
+ //variáveis
+ let nome 
+ let idade
+ let vip
+
+ nome = "Alex Lima"
+ console.log(typeof(nome))
+ console.log(nome)
+
+ idade = "20"
+ console.log(typeof(idade))
+ console.log(idade)
+
+ vip = true
+ console.log(typeof(vip))
+ console.log(vip)
