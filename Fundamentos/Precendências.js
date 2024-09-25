e4305eef5f55cd44d5fa6f2499906028e1b3e073
Alex Lima <alexsantoslima007@gmail.com>
2024-09-24 22:22:42 -0300

    atualização

---
+++ Fundamentos/Precendências.js
@@ -0,0 +1,12 @@
+/**
+ * Operadores aritiméticos
+ * @author Alex Lima
+ * Regra de precedências
+ */
+console.clear()
+console.log("Prioridade     |    simbologia")
+console.log("   1º          |        ( )")
+console.log("   2º          |         *")
+console.log("   3º          |         /")
+console.log("   4°          |         +")
+console.log("   5º          |         -")
---
+++ Fundamentos/sintaxe1.js
@@ -0,0 +1,19 @@
+/**
+ * Sintaxe Básica da linguagem JS
+ * @author Alex Lima
+ */
+
+//Limpar a tela o console
+console.clear()
+
+// JS - Sentença ; opcional
+console.log("Hello World")
+
+// concatenação (unir textos e variáveis)
+let nome = "Alex Lima"
+console.log(`Nome: ${nome}`)
+console.log("Nome: " + nome) //não recomendado
+
+// arrendondamento (casas decimais)
+let media = 5.789
+console.log(`Média: ${media.toFixed(1)}`)
