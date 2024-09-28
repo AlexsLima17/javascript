40d5e54a555b50972b6d25b83622e752ec3d8125
Alex Lima <alexsantoslima007@gmail.com>
2024-09-27 21:46:58 -0300

    real em dolar

--- Exercícios/Desafio.js
+++ Exercícios/Desafio.js
@@ -3,29 +3,27 @@
  * @author Alex Lima & Kevin Teodoro
  */
 
-const readline = require('readline').createInterface({ 
-    input: process.stdin, 
-    output: process.stdout 
-}); 
-    
-readline.question('Qual o valor em Reais (R$)? ', (valorReais) => { 
-    readline.question('Qual a taxa de câmbio (R$ para US$)? ', (taxaCambio) => { 
-        
-        // Converte os valores para números 
-        const reais = parseFloat(valorReais); 
-        const taxa = parseFloat(taxaCambio); 
-        
-        // Calcula o valor em dólares 
-        const valorDolares = reais / taxa;
 
-         // Exibe o resultado 
-         console.log(`O valor em dólares (US$) é: ${valorDolares.toFixed(2)}`);
+// importa a biblioteca read-linesync
+const input = require('readline-sync')
 
-        readline.close();
-    
-    }); 
+// variáveis
+let Real, Taxa, Dolar
+console.clear()
+console.log("Calculo do Real para Dolar")
 
-}); 
+// entrada 
+// No console entradas (input) numéricas precisam ser convertidas com uso métodos Number()
+Real = Number(input.question("Digite o valor em real: "))
+Taxa = Number(input.question("Digite o valor da taxa de cambio: "))
+
+
+//processamneto 
+Dolar = (Real / Taxa )
+
+//saída
+console.log(`Dolar: ${Dolar.toFixed(1)}`)
+
 
 
 
